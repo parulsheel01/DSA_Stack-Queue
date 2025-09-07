147f6d27858839ec82560749445c80018ce6212c
parulsheel01 <sheelparul7017@gmail.com>
2025-09-08 02:01:43 +0530

    Create NextGreaterEl.java

---
+++ NextGreaterEl.java
@@ -0,0 +1,22 @@
+class NextGreaterEl {
+    public int[] nextGreaterElements(int[] nums) {
+        int n = nums.length;
+        Stack<Integer> st = new Stack<>();
+        int[] nge = new int[n];
+        for(int i=2*n-1;i>=0;i--){
+            while(!st.isEmpty() && st.peek() <= nums[i%n]){
+                st.pop();
+            }
+            if(i < n){
+                if(st.isEmpty()){
+                    nge[i] = -1;
+                }
+                else{
+                    nge[i] = st.peek();
+                }
+            }
+            st.push(nums[i%n]);
+        }
+        return nge;
+    }
+}
